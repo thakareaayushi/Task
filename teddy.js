3f558857d8de6b2fbd7f58edf9cc837ee64b7ca3
Aayushi Thakare <152911856+thakareaayushi@users.noreply.github.com>
2024-01-19 16:33:51 +0530

    Add files via upload

---
+++ teddy.js
@@ -0,0 +1,23 @@
+function red(){
+   var x= document.getElementById("image").src="https://m.media-amazon.com/images/I/51t9tgGWfRL._SX300_SY300_QL70_FMwebp_.jpg"
+    document.getElementById("price-box").innerHTML="MRP 2,199"
+}
+function black(){
+    var x= document.getElementById("image").src="https://m.media-amazon.com/images/I/61OorqJvxfL._SY450_.jpg"
+     document.getElementById("price-box").innerHTML="MRP 1,199"
+ }
+ function green(){
+    var x= document.getElementById("image").src="https://m.media-amazon.com/images/I/41hzS+4UJOL._SX450_.jpg"
+     document.getElementById("price-box").innerHTML="MRP 1,199"
+ }
+ function blue(){
+    var x= document.getElementById("image").src="https://m.media-amazon.com/images/I/51OcJurr21L._SX300_SY300_QL70_FMwebp_.jpg"
+     document.getElementById("price-box").innerHTML="MRP 1,099"
+ }
+ function mult(){
+    var x= document.getElementById("image").src="https://m.media-amazon.com/images/I/41mQS3pqBcL._SX300_SY300_QL70_FMwebp_.jpg"
+     document.getElementById("price-box").innerHTML="MRP 1,099"
+ }
+ function cart(){
+    var cart=document.getElementById("cart").innerHTML.src="https://m.media-amazon.com/images/I/51t9tgGWfRL._SX300_SY300_QL70_FMwebp_.jpg"
+ }
